3d384986229e52689ce90f8903d1b11e6f5fbc18
jspearus <35698087+jspearus@users.noreply.github.com>
2019-08-07 15:41:17 -0500

    Styled the Post scaffolding to use Bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 20190807005046) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
