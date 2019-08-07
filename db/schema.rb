d44df40646e4c511f4d9a664e1c0ae08ac3a2b77
jspearus <35698087+jspearus@users.noreply.github.com>
2019-08-07 17:17:54 -0500

    Added Image support for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190807005046) do
+ActiveRecord::Schema.define(version: 20190807214131) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "kind"
   end
 
 end
